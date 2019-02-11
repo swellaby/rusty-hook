cfde3aade98b4d6e2dadbecf911dfa25320a9064
calebcartwright <caleb.cartwright@outlook.com>
2019-02-10 22:05:32 -0600

    chore: tweaked dev_setup scripts

--- scripts/dev_setup.ps1
+++ scripts/dev_setup.ps1
@@ -53,4 +53,5 @@ if (-NOT (Get-Command 'rusty-hook' -ErrorAction SilentlyContinue))
 
 Write-Host "Environment successfully configured!"
 cd (Get-Item $PSScriptRoot).Parent.FullName
+rusty-hook init
 cargo test --bin rusty-hook
--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -5,8 +5,6 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd .. 
 if ! command -v rustc >/dev/null 2>&1; then
   curl https://sh.rustup.rs -sSf | sh -s -- -y
   PATH=$PATH:$HOME/.cargo/bin
-  source ~/.profile
-  source ~/.bashrc
 fi
 
 if ! command -v cargo-clippy >/dev/null 2>&1; then
@@ -14,9 +12,10 @@ if ! command -v cargo-clippy >/dev/null 2>&1; then
 fi
 
 if ! command -v rusty-hook >/dev/null 2>&1; then
-  cargo install --path $ROOT_DIR
+  cargo install --path "$ROOT_DIR"
 fi
 
 echo "Environment successfully configured!"
-cd $ROOT_DIR
+cd "$ROOT_DIR" || exit 1
+rusty-hook init
 cargo test --bin rusty-hook
