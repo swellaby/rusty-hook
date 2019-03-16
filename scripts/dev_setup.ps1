4e389fc72dec2f2df60ca9f2e80a79d22b71a775
calebcartwright <caleb.cartwright@outlook.com>
2019-03-16 16:38:21 -0500

    chore: trying to fix windows setup script

--- scripts/dev_setup.ps1
+++ scripts/dev_setup.ps1
@@ -20,21 +20,17 @@ if (-NOT (Get-Command 'rustc' -ErrorAction SilentlyContinue))
     Write-Host 'Rust not found...'
     Write-Host "Checking for pre-requisite C++ build tools..."
 
-    # Check for C++ build tool pre-requisites
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    Install-Module VSSetup -Scope CurrentUser
-    $installationPaths = Get-VSSetupInstance | Select -ExpandProperty InstallationPath
-    foreach ($installationPath in $installationPaths)
+    # Check for C++ build tool pre-requisites using vswhere since the PS Module and Com approach are
+    # too fragile in proxy environments
+    # https://devblogs.microsoft.com/cppblog/finding-the-visual-c-compiler-tools-in-visual-studio-2017/
+    if (-NOT (Get-Command 'vswhere' -ErrorAction SilentlyContinue))
     {
-        if ((Get-ChildItem -Recurse -Path "$installationPath" -Filter cl.exe | Select-Object -First 1))
-        {
-            $buildToolsInstalled = $true
-            break
-        }
+        Write-Host "Installing vswhere tool to scan for C++ build tools..."
+        cinst -y vswhere
     }
 
-    if (!$buildToolsInstalled)
+    $product = vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+    if (!$product)
     {
         Write-Host "C++ build tools not found. Starting install now. This might take a while..."
         cinst -y --timeout 14400 --force visualstudio2017-workload-vctools
@@ -42,6 +38,27 @@ if (-NOT (Get-Command 'rustc' -ErrorAction SilentlyContinue))
         Start-Sleep -Seconds 15
         Exit
     }
+    # Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    # Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    # Install-Module VSSetup -Scope CurrentUser
+    # $installationPaths = Get-VSSetupInstance | Select -ExpandProperty InstallationPath
+    # foreach ($installationPath in $installationPaths)
+    # {
+    #     if ((Get-ChildItem -Recurse -Path "$installationPath" -Filter cl.exe | Select-Object -First 1))
+    #     {
+    #         $buildToolsInstalled = $true
+    #         break
+    #     }
+    # }
+
+    # if (!$buildToolsInstalled)
+    # {
+    #     Write-Host "C++ build tools not found. Starting install now. This might take a while..."
+    #     cinst -y --timeout 14400 --force visualstudio2017-workload-vctools
+    #     Write-Host "C++ build tools install finished. You should restart your machine to complete the install, and then re-run this script. Script will exit in 15 seconds..."
+    #     Start-Sleep -Seconds 15
+    #     Exit
+    # }
 
     Write-Host "Pre-requisite C++ build tools detected..."
     Write-Host "Installing Rust now..."
