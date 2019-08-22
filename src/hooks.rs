5be09ed79c4d68287e9aff99d14a7fd074325467
calebcartwright <caleb.cartwright@outlook.com>
2019-08-21 19:30:36 -0500

    chore: satiate kcov and clippy

--- src/hooks.rs
+++ src/hooks.rs
@@ -35,8 +35,11 @@ const HOOK_NAMES: [&str; 19] = [
 const CLI_SCRIPT_NAME: &str = "cli.sh";
 const SEMVER_SCRIPT_NAME: &str = "semver.sh";
 
+// For unknown reasons, kcov is reporting an uncovered line for the closing `}`
+// when using the idiomatic expression.
+#[allow(clippy::needless_return)]
 fn get_hook_file_contents() -> String {
-    String::from(HOOK_FILE_TEMPLATE).replace("{{VERSION}}", VERSION)
+    return String::from(HOOK_FILE_TEMPLATE).replace("{{VERSION}}", VERSION);
 }
 
 fn get_cli_script_file_contents() -> String {
@@ -55,8 +58,11 @@ fn get_cli_script_file_contents() -> String {
         .replace("{{MINIMUM_ALLOW_PRE}}", minimum_allow_pre)
 }
 
+// For unknown reasons, kcov is reporting an uncovered line for the closing `}`
+// when using the idiomatic expression.
+#[allow(clippy::needless_return)]
 fn get_semver_script_file_contents() -> String {
-    String::from(HOOK_SEMVER_SCRIPT_FILE_TEMPLATE).replace("{{VERSION}}", VERSION)
+    return String::from(HOOK_SEMVER_SCRIPT_FILE_TEMPLATE).replace("{{VERSION}}", VERSION);
 }
 
 fn get_file_path(root_directory_path: &str, hooks_directory: &str, file: &str) -> String {
