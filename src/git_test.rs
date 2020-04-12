b95deaa383380bacbe0a658162fbd196d5d83cfd
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-04-12 14:10:47 -0500

    feat: use new cmd runner in git mod

--- src/git_test.rs
+++ src/git_test.rs
@@ -8,23 +8,24 @@ mod get_root_directory_path_tests {
     fn uses_git_rev_parse_top_level_command() {
         let exp = "/usr/me/foo";
         let target_dir = "";
-        let run_command = |cmd: &str, dir: Option<&str>| {
-            if cmd == "git rev-parse --show-toplevel" && dir == Some(target_dir) {
-                Ok(String::from(exp))
+        let run_command = |cmd: &str, dir: Option<&str>, stream_io: bool| {
+            if cmd == "git rev-parse --show-toplevel" && dir == Some(target_dir) && !stream_io {
+                Ok(Some(String::from(exp)))
             } else {
-                Ok(String::from(""))
+                Ok(None)
             }
         };
         let act = get_root_directory_path(run_command, Some(&target_dir));
-        assert_eq!(act.unwrap(), exp);
+        assert_eq!(act.unwrap(), Some(String::from(exp)));
     }
 
     #[test]
     fn returns_error_on_command_error() {
         let exp_err = "Ah!";
-        let run_command = |_cmd: &str, _dir: Option<&str>| Err(String::from(exp_err));
+        let run_command =
+            |_cmd: &str, _dir: Option<&str>, _stream_io: bool| Err(Some(String::from(exp_err)));
         let act = get_root_directory_path(run_command, None);
-        assert_eq!(act, Err(String::from(exp_err)));
+        assert_eq!(act, Err(Some(String::from(exp_err))));
     }
 }
 
@@ -36,23 +37,24 @@ mod get_hooks_directory_tests {
     fn uses_git_hooks_path_command() {
         let exp = ".git/hooks";
         let target_dir = "";
-        let run_command = |cmd: &str, dir: Option<&str>| {
-            if cmd == "git rev-parse --git-path hooks" && dir == Some(target_dir) {
-                Ok(String::from(exp))
+        let run_command = |cmd: &str, dir: Option<&str>, stream_io: bool| {
+            if cmd == "git rev-parse --git-path hooks" && dir == Some(target_dir) && !stream_io {
+                Ok(Some(String::from(exp)))
             } else {
-                Ok(String::from(""))
+                Ok(None)
             }
         };
         let act = get_hooks_directory(run_command, &target_dir);
-        assert_eq!(act.unwrap(), exp);
+        assert_eq!(act.unwrap(), Some(String::from(exp)));
     }
 
     #[test]
     fn returns_error_on_command_error() {
         let exp_err = "failed";
-        let run_command = |_cmd: &str, _dir: Option<&str>| Err(String::from(exp_err));
+        let run_command =
+            |_cmd: &str, _dir: Option<&str>, _stream_io: bool| Err(Some(String::from(exp_err)));
         let act = get_hooks_directory(run_command, "");
-        assert_eq!(act, Err(String::from(exp_err)));
+        assert_eq!(act, Err(Some(String::from(exp_err))));
     }
 }
 
@@ -63,7 +65,7 @@ mod setup_hooks_tests {
     #[test]
     fn errors_when_hooks_directory_unknown() {
         let exp_err = "Failure determining git hooks directory";
-        let run_command = |_cmd: &str, _dir: Option<&str>| Err(String::from(""));
+        let run_command = |_cmd: &str, _dir: Option<&str>, _stream_io: bool| Err(None);
         let write_file = |_path: &str, _contents: &str, _x: bool| Ok(());
         let result = setup_hooks(run_command, write_file, "");
         assert_eq!(result, Err(String::from(exp_err)));
@@ -71,8 +73,9 @@ mod setup_hooks_tests {
 
     #[test]
     fn errors_when_hook_write_fails() {
-        let run_command =
-            |_cmd: &str, _dir: Option<&str>| Ok(String::from("/usr/repos/foo/.git/hooks"));
+        let run_command = |_cmd: &str, _dir: Option<&str>, _stream_io: bool| {
+            Ok(Some(String::from("/usr/repos/foo/.git/hooks")))
+        };
         let write_file = |_path: &str, _contents: &str, _x: bool| Err(String::from(""));
         let result = setup_hooks(run_command, write_file, "");
         assert_eq!(result, Err(String::from(hooks::HOOK_CREATION_ERROR)));
@@ -82,7 +85,7 @@ mod setup_hooks_tests {
     fn creates_all_hooks() {
         let root_dir = "/usr/repos/foo";
         let git_hooks = ".git/hooks";
-        let run_command = |_c: &str, _d: Option<&str>| Ok(String::from(git_hooks));
+        let run_command = |_c: &str, _d: Option<&str>, _s: bool| Ok(Some(String::from(git_hooks)));
         let write_file = |_p: &str, _c: &str, _x: bool| Ok(());
         let result = setup_hooks(run_command, write_file, root_dir);
         assert_eq!(result, Ok(()));
